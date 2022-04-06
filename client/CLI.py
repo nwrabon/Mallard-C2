d44abe7b9d715b342a609d79d7a46c4f7769c4d8
Nick Rabon <nwrabon@gmail.com>
2022-04-06 14:55:47 -0400

    fixed that bad juju

--- client/CLI.py
+++ client/CLI.py
@@ -1,10 +1,9 @@
-from client.victim import recv_msg
 from rich import print as rprint
 from rich.table import Table
 from rich.prompt import Prompt as prompt
-import clipboard
 
 import socket
+import struct
 import pickle
 import codecs
 import re as r
@@ -13,13 +12,33 @@ from sys import platform
 
 import generator
 
+def recvall(sock, n):
+    # Helper function to recv n bytes or return None if EOF is hit
+    data = bytearray()
+    while len(data) < n:
+        packet = sock.recv(n - len(data))
+        if not packet:
+            return None
+        data.extend(packet)
+    return data
+
+
+def recv_msg(sock):
+    # Read message length and unpack it into an integer
+    raw_msglen = recvall(sock, 4)
+    if not raw_msglen:
+        return None
+    msglen = struct.unpack('!I', raw_msglen)[0]
+    # Read the message data
+    return recvall(sock, msglen)
+
 
 # Client Side 
 
 # TODO: conn keepalive
 # sock.ioctl(socket.SIO_KEEPALIVE_VALS, (1, 60000, 30000))
 # Create Client session; listen to server
-server_addr = ('198.21.242.142', 1338)
+server_addr = ('192.168.205.199', 1338)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     sock.connect(server_addr)
